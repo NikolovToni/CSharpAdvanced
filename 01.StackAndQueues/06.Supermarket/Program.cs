ac22f292552ca9035cb76ebcac2beb8156944815
NikolovToni <nikolov.s.toni@gmail.com>
2021-09-18 22:11:34 +0300

    Sumermarket task solved

--- 01.StackAndQueues/06.Supermarket/Program.cs
+++ 01.StackAndQueues/06.Supermarket/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _06.Supermarket
 {
@@ -6,7 +8,31 @@ namespace _06.Supermarket
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            Queue<string> clients = new Queue<string>();
+            int counter = 0;
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    for (int i = 0; i < counter; i++)
+                    {
+                        Console.WriteLine(clients.Dequeue()); ;
+                    }
+                    counter = 0;
+                }
+                else
+                {
+                    clients.Enqueue(input);
+                    counter++;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{clients.Count} people remaining.");
         }
     }
 }
