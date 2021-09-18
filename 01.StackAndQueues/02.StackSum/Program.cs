351b3778c9b702091165180640825a902db8ba28
NikolovToni <nikolov.s.toni@gmail.com>
2021-09-18 17:26:34 +0300

    StackSum task solved

--- 01.StackAndQueues/02.StackSum/Program.cs
+++ 01.StackAndQueues/02.StackSum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _02.StackSum
 {
@@ -6,7 +8,38 @@ namespace _02.StackSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(' ');
+
+            Stack<int> numbers = new Stack<int>();
+            foreach (var item in input)
+            {
+                numbers.Push(int.Parse(item));
+            }
+
+            string command = Console.ReadLine().ToLower();
+            while (command != "end")
+            {
+                string[] parts = command.Split(' ');
+                if (parts[0] == "add")
+                {
+                    numbers.Push(int.Parse(parts[1]));
+                    numbers.Push(int.Parse(parts[2]));
+                }
+                else if (parts[0] == "remove")
+                {
+                    if (int.Parse(parts[1]) < numbers.Count)
+                    {
+                        for (int i = 0; i < int.Parse(parts[1]); i++)
+                        {
+                            numbers.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+
+            int sum = numbers.Sum();
+            Console.WriteLine($"Sum: {sum}");
         }
     }
 }
