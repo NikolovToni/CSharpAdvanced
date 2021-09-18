06f26f3446987549f9b0da337cab92d86378b884
NikolovToni <nikolov.s.toni@gmail.com>
2021-09-18 17:59:29 +0300

    SimpleCalculator solved

--- 01.StackAndQueues/03.SimpleCalculator/Program.cs
+++ 01.StackAndQueues/03.SimpleCalculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _03.SimpleCalculator
 {
@@ -6,7 +8,30 @@ namespace _03.SimpleCalculator
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
 
+            Stack<string> equation = new Stack<string>(input);
+
+            while (equation.Count > 1)
+            {
+                int a = int.Parse(equation.Pop());
+                string @operator = equation.Pop();
+                int b = int.Parse(equation.Pop());
+
+                if (@operator == "+")
+                {
+                    equation.Push((a + b).ToString());
+                }
+                else
+                {
+                    equation.Push((a - b).ToString());
+                }
+            }
+
+            Console.WriteLine(equation.Pop());
         }
     }
 }
