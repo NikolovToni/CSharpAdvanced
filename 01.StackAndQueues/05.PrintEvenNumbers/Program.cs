baca7e201e4e543cac8d937b92dc0bcf931768df
NikolovToni <nikolov.s.toni@gmail.com>
2021-09-18 19:41:00 +0300

    PrintEvenNumbers solved

---
+++ 01.StackAndQueues/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queueNumbers = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    queueNumbers.Enqueue(numbers[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queueNumbers));
+        }
+    }
+}
