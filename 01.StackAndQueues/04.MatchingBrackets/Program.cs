ee3a7f3671c409a3a75e447688043733525d05a2
NikolovToni <nikolov.s.toni@gmail.com>
2021-09-18 19:16:24 +0300

    MatchingBrackets solved

--- 01.StackAndQueues/04.MatchingBrackets/Program.cs
+++ 01.StackAndQueues/04.MatchingBrackets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04.MatchingBrackets
 {
@@ -6,6 +7,23 @@ namespace _04.MatchingBrackets
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+
+            Stack<int> indices = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    indices.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int openBracketIndex = indices.Pop();
+                    int closedBrackerIndex = i;
+                    Console.WriteLine(input.Substring(openBracketIndex, closedBrackerIndex - openBracketIndex + 1));
+                }
+            }
+
         }
     }
 }
