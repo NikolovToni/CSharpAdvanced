7224f2ab20f1abf7df596956d994cbe791d16141
NikolovToni <nikolov.s.toni@gmail.com>
2021-09-22 17:05:21 +0300

    SumMatrixElements task solved

--- 02.MultidimensionalArrays/01.SumMatrixElements/Program.cs
+++ 02.MultidimensionalArrays/01.SumMatrixElements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01.SumMatrixElements
 {
@@ -6,7 +8,36 @@ namespace _01.SumMatrixElements
     {
         static void Main(string[] args)
         {
-            
+            int[] inputLine = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = inputLine[0];
+            int cols = inputLine[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] line = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+            int sumMatrix = 0;
+            foreach (var number in matrix)
+            {
+                sumMatrix += number;
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sumMatrix);
         }
     }
 }
